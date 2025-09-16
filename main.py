334e9de22b3b146ebcabac625ac23815703aa444
Codemigod-666 <vikay434@gmail.com>
2025-09-16 18:25:35 +0530

    Update and Delete products without database

--- main.py
+++ main.py
@@ -37,3 +37,28 @@ def get_product_by_id(product_id: int):
 def create_product(product: Product):
     products.append(product)
     return {"message": "Product created successfully", "product" : product}
+
+
+# PUT - update prodcuts 
+@app.put("/products/{product_id}")
+def update_products(product_id: int, product: Product):
+    for i in range(len(products)):
+        if products[i].id == product_id:
+            products[i] = product
+            return { "message": "Product updated successfully", "product": product}
+
+    return {"error": "Product not found"}
+
+
+# DELETE - delete product 
+@app.delete("/products/{product_id}")
+def delete_product(product_id: int):
+    for i in range(len(products)):
+        if products[i].id == product_id:
+            del products[i]
+            return {"message": "Product deleted successfully"}
+        
+    return {"error": "Products not found"}
+
+
+    
