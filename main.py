4e199f28adc934233c534ab8d286d0a039cf030d
Codemigod-666 <vikay434@gmail.com>
2025-09-17 23:54:49 +0530

    adding the frontend url to the cors middleware

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ models.Base.metadata.create_all(bind=engine)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://f-tutorial.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
